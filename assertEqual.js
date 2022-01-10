11ead6c51cd934f12901b73eca319fe1f7f48004
Seamus McGill <seamusmcgill@gmail.com>
2022-01-10 16:42:25 -0500

    Flipped function so checking whether equal comes first

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
-  } else {
+  if (actual === expected) {
     console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
   }
 };
 
