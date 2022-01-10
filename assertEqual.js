2896b86f233be2bcb0d980e4346464f507d26f0c
Seamus McGill <seamusmcgill@gmail.com>
2022-01-10 16:33:27 -0500

    Wrote a function to replace console.assert

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(String.fromCodePoint(0x274C) + " Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log(String.fromCodePoint(0x2705) + " Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Seamus", "Seamus");
+assertEqual(10, 12);
