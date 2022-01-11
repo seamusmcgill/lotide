481c32be7643cc9f6bf91ac1c1f1afb273fb9307
Seamus McGill <seamusmcgill@gmail.com>
2022-01-11 16:23:46 -0500

    Created function to print result of array equivalency check

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    // If any element isn't equal the array is not equal
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  // Check if arrays are equivalent or not
+  if (eqArrays(array1, array2)) {
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${array1} === ${array2}`);
+  }
+};
+
+// TEST CASES
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, "two", 3], [1, 2, 3]);
