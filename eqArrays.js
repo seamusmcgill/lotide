25dcd4bcab9e604463f56b5fa63f8c855acf6da4
Seamus McGill <seamusmcgill@gmail.com>
2022-01-11 16:13:11 -0500

    Created a function to compare equality of two arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,27 @@
 const assertEqual = function(actual, expected) {
+  // Output passing/failing messages (emojis included)
   if (actual === expected) {
     console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
   }
 };
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    // If any element isn't equal the array is not equal
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CASES
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays([], []));
+console.log(eqArrays(["hello", 2, "four"], ["hello", 2, "four"]));
+console.log(eqArrays([1, "2", 3], [1, 2, 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
