56b70ceebbeb50ab0a6f1a4b29c5603c5f0c8bb7
Seamus McGill <seamusmcgill@gmail.com>
2022-01-11 19:24:37 -0500

    Created a function to flatten an array

---
+++ flatten.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    // If any element isn't equal the array is not equal
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  // Check if arrays are equivalent or not
+  if (eqArrays(array1, array2)) {
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${array1} === ${array2}`);
+  }
+};
+
+const flatten = function(array) {
+  // Create empty flattened array
+  let flattenedArray = [];
+  // Loop through array elements
+  for (const element of array) {
+    // Check if element is a subarray
+    if (Array.isArray(element)) {
+      // Loop through subarray and push each element to flattened array
+      for (const subElement of element) {
+        flattenedArray.push(subElement);
+      }
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
+};
+
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [ 1, 2, 3, 4, 5, 6 ]);
