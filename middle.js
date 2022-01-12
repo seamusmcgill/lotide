ad7cc5f6e5a64d656995def14f13308974fbc5d2
Seamus McGill <seamusmcgill@gmail.com>
2022-01-12 09:37:20 -0500

    Added more test cases

--- middle.js
+++ middle.js
@@ -31,7 +31,9 @@ const middle = function(array) {
   return output;
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1]), []);
