a617f77934a5feeca215f4a87597fe942451df51
Seamus McGill <seamusmcgill@gmail.com>
2022-01-12 09:11:50 -0500

    Fixed function to solve all test cases; switched from forEach to for loops; switched from splicing orignal array to returning new one

--- without.js
+++ without.js
@@ -18,17 +18,23 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 const without = function(source, itemsToRemove) {
+  // Create empty output array
+  let output = [];
   // Loop through source and itemsToRemove arrays
-  source.forEach((element, index) => {
-    itemsToRemove.forEach(item => {
+  source.forEach(element => {
+    for (let i = 0; i < itemsToRemove.length; i++) {
       // If element is in itemsToRemove splice it from the array
-      if (element === item) {
-        source.splice(index, 1);
+      if (element === itemsToRemove[i]) {
+        break;
       }
-    });
+      if (i === itemsToRemove.length - 1) {
+        output.push(element);
+      }
+    }
   });
-  return source;
+  return output;
 };
 
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 assertArraysEqual(without(["Hey", "there", "champ"], ["champ", 4, "3"]), ["Hey", "there"]);
+assertArraysEqual(without([1, 4, 2, 3, 2, 4], [2, 4]), [1, 3]);
