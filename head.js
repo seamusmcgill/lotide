8be1d47fcd011605a4a20b34bebe6e92eb52cf71
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 09:09:18 -0500

    Removed assertEqual definition and instead required the function from assertEqual.js

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  // Output passing/failing messages (emojis included)
-  if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   // Check if array is empty
