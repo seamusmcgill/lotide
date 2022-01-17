e0c8d1aec9259c18313527261bfa3cad71d99434
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 10:14:26 -0500

    Removed old test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -19,11 +19,3 @@ describe("#middle", () => {
   });
 
 });
-// TEST CODE
-
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1]), []);
