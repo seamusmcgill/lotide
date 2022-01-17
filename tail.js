55ed369c74b8db95e435df9230e3cf1e1f5169a2
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 09:20:35 -0500

    Removed assertEqual function and moved test code to own file

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const tail = function(array) {
   // Check if array is empty
   if (!array.length) {
@@ -20,10 +12,4 @@ const tail = function(array) {
   return arrayTail;
 };
 
-// TEST CASES
-let words = ["Lighthouse", "Labs", "Web", "Development", "Bootcamp"];
-let result = tail(words);
-assertEqual(result.length, words.length - 1);
-for (let j = 0; j < words.length - 1; j++) {
-  assertEqual(result[j], words[j + 1]);
-}
+module.exports = tail;
