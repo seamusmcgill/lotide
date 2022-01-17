7ae69ca298c3301432791712ecc5f969c1ed171f
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 18:23:27 -0500

    Refactored eqArrays.js to create a separate test file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,37 @@
-const assertEqual = require("../assertEqual");
+const expect = require("chai").expect;
 const eqArrays = require("../eqArrays");
 
 // TEST CASES
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([], []));
-console.log(eqArrays(["hello", 2, "four"], ["hello", 2, "four"]));
-console.log(eqArrays([1, "2", 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, [2, [3, 4, 5, [1], 2], 33], 4], [1, [2, [3, 4, 5, [1], 2], 33], 4]), true);
+describe("#eqArrays", () => {
+  it("should return true when equivalent arrays are passed in", () => {
+    let result = eqArrays([1, 2, 3], [1, 2, 3]);
+    let expected = true;
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should return true when equivalent nested arrays are passed in", () => {
+    let result = eqArrays([1, [2, [3, 4, 5, [1], 2], 33], 4], [1, [2, [3, 4, 5, [1], 2], 33], 4]);
+    let expected = true;
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should return true when two empty arrays are passed in", () => {
+    let result = eqArrays([], []);
+    let expected = true;
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should return false when non-matching arrays are passed in", () => {
+    let result = eqArrays([1, "2", 3], [1, 2, 3]);
+    let expected = false;
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should return false when non-equivalent nested arrays are passed in", () => {
+    let result = eqArrays([1, [2, [3, 4], 5], 6], [1, [2, [3, 3], 5], 6]);
+    let expected = false;
+    expect(result).to.deep.equal(expected);
+  });
+
+});
