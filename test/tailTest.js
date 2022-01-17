c57a62735e1c51113d116b97083ddbb94fad2a1b
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 13:04:32 -0500

    Removed test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -14,11 +14,3 @@ describe("#tail", () => {
     assert.deepEqual(tail([1]), []);
   });
 });
-
-// // TEST CASES
-// let words = ["Lighthouse", "Labs", "Web", "Development", "Bootcamp"];
-// let result = tail(words);
-// assertEqual(result.length, words.length - 1);
-// for (let j = 0; j < words.length - 1; j++) {
-//   assertEqual(result[j], words[j + 1]);
-// }
