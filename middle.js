e47db09368fad72830782c8ece84344f8712d4b5
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 09:33:32 -0500

    Added separate test file

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(array1, array2) {
-  // Check if the arrays are different lengths
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    // If any element isn't equal the array is not equal
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  // Check if arrays are equivalent or not
-  if (eqArrays(array1, array2)) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${array1} === ${array2}`);
-  }
-};
-
 const middle = function(array) {
   let output = [];
   if (array.length < 3) {
@@ -35,9 +12,4 @@ const middle = function(array) {
   return output;
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
