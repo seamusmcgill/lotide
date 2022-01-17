e6be2e81a0d4d76ae16be6c63203b634ab2649b6
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 16:35:42 -0500

    Fixed equal spelling error

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -5,7 +5,7 @@ describe("#flatten", () => {
   it("returns [ 1, 2, 3, 4, 5, 6 ] for [1, 2, [3, 4], 5, [6]]", () => {
     let result = flatten([1, 2, [3, 4], 5, [6]]);
     let expected = [ 1, 2, 3, 4, 5, 6 ];
-    expect(result).to.deep.beequal(expected);
+    expect(result).to.deep.equal(expected);
   });
 
   it("returns [] for []", () => {
