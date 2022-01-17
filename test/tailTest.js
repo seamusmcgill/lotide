ecfb2139116764c94b058f610c3b3f3aae7ccb6b
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 10:14:51 -0500

    Refactored tailTest to use mocha and chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,24 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-// TEST CASES
-let words = ["Lighthouse", "Labs", "Web", "Development", "Bootcamp"];
-let result = tail(words);
-assertEqual(result.length, words.length - 1);
-for (let j = 0; j < words.length - 1; j++) {
-  assertEqual(result[j], words[j + 1]);
-}
+describe("#tail", () => {
+  it("should return undefined for []", () => {
+    assert.deepEqual(tail([]), undefined);
+  });
+
+  it("should return [Labs, Bootcamp] for [Lighthouse, Labs, Bootcamp]", () => {
+    assert.deepEqual(tail(["Lighthouse", "Labs", "Bootcamp"]), ["Labs", "Bootcamp"]);
+  });
+
+  it("should return [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
+
+// // TEST CASES
+// let words = ["Lighthouse", "Labs", "Web", "Development", "Bootcamp"];
+// let result = tail(words);
+// assertEqual(result.length, words.length - 1);
+// for (let j = 0; j < words.length - 1; j++) {
+//   assertEqual(result[j], words[j + 1]);
+// }
