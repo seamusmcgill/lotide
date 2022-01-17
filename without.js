78b02b69e8aa14d95fd6bd322aabd7a73fc553ed
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 13:49:56 -0500

    Added module.exports to file to use function across library

--- without.js
+++ without.js
@@ -1,26 +1,3 @@
-const eqArrays = function(array1, array2) {
-  // Check if the arrays are different lengths
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    // If any element isn't equal the array is not equal
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  // Check if arrays are equivalent or not
-  if (eqArrays(array1, array2)) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${array1} === ${array2}`);
-  }
-};
-
 const without = function(source, itemsToRemove) {
   // Create empty output array
   let output = [];
@@ -39,6 +16,8 @@ const without = function(source, itemsToRemove) {
   return output;
 };
 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-assertArraysEqual(without(["Hey", "there", "champ"], ["champ", 4, "3"]), ["Hey", "there"]);
-assertArraysEqual(without([1, 4, 2, 3, 2, 4], [2, 4]), [1, 3]);
+module.exports = without;
+
+// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+// assertArraysEqual(without(["Hey", "there", "champ"], ["champ", 4, "3"]), ["Hey", "there"]);
+// assertArraysEqual(without([1, 4, 2, 3, 2, 4], [2, 4]), [1, 3]);
