eb82d0e76d577e2714fd4d65af8cb5b3d44ee832
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 09:55:24 -0500

    Updated test file to run mocha tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), "Array");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head(["Hello"]), "Hello");
+// assertEqual(head([]), "Array");
