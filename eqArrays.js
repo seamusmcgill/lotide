32c76f7e7caf07f7541a1ee904fcceeed75d028a
Seamus McGill <seamusmcgill@gmail.com>
2022-01-17 09:24:32 -0500

    Moved test code to separate file and exported function

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  // Output passing/failing messages (emojis included)
-  if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   // Check if the arrays are different lengths
   if (array1.length !== array2.length) {
@@ -31,12 +22,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// TEST CASES
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([], []));
-console.log(eqArrays(["hello", 2, "four"], ["hello", 2, "four"]));
-console.log(eqArrays([1, "2", 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, [2, [3, 4, 5, [1], 2], 33], 4], [1, [2, [3, 4, 5, [1], 2], 33], 4]), true);
+module.exports = eqArrays;
